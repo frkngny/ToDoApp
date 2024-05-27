e63090d4bce1639864a3b87654d89885726d184c
frkngny <frkang35@gmail.com>
2024-05-27 20:31:48 +0300

    start todoapp project

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from .forms import RegisterForm
+
+
+class UserRegister(CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    
+    def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super().get(request, *args, **kwargs)
+
+class UserLogin(LoginView):
+    template_name = 'accounts/login.html'
